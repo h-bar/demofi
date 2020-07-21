513815e15415f124e1ceb3b514cad6030cbdc697
Yan Zhang <663yan@gmail.com>
2020-07-20 23:48:00 -0400

    add more styling

--- src/App.js
+++ src/App.js
@@ -82,7 +82,7 @@ class App extends React.Component {
   render() {
     return (
       <div className="App">
-        <div className="jumbotron">
+        <div className="jumbotron text-center">
         <h1 className="display-4">Word Tagging Demo</h1>
           <p className="lead">This is a simple Demo for classification and tagging tasks</p>
           <hr className="my-4" />
@@ -91,26 +91,33 @@ class App extends React.Component {
             <a className="btn btn-primary btn-lg" href="#" role="button">Github</a>
           </p>
         </div>
-        <div className="container">
-          <div>
-            <div style={{height: '200px'}} className="mb-4"> 
-              <TextAreaIn onUpdate={this.updateData} name="data.content" placeholder="Input some data"></TextAreaIn>  
-            </div>
-            <div  className="mb-4">
-              <OptionBtnIn onUpdate={this.updateData} name="param.content" options={["sdfd", "sdsf", "sdfs", "dsfsfdfs", "dsfsdfsdf", "dfsfdfs", "sdfdsf", "dsdfsdf", "sdfd", "sdsf", "sdfs", "dsfsfdfs", "dsfsdfsdf", "dfsfdfs", "sdfdsf","sdfd", "sdsf", "sdfs", "dsfsfdfs", "dsfsdfsdf", "dfsfdfs", "sdfdsf","sdfd", "sdsf", "sdfs", "dsfsfdfs", "dsfsdfsdf", "dfsfdfs", "sdfdsf","sdfd", "sdsf", "sdfs", "dsfsfdfs", "dsfsdfsdf", "dfsfdfs", "sdfdsf","sdfd", "sdsf", "sdfs", "dsfsfdfs", "dsfsdfsdf", "dfsfdfs", "sdfdsf","sdfd", "sdsf", "sdfs", "dsfsfdfs", "dsfsdfsdf", "dfsfdfs", "sdfdsf","sdfd", "sdsf", "sdfs", "dsfsfdfs", "dsfsdfsdf", "dfsfdfs", "sdfdsf","sdfd", "sdsf", "sdfs", "dsfsfdfs", "dsfsdfsdf", "dfsfdfs", "sdfdsf","sdfd", "sdsf", "sdfs", "dsfsfdfs", "dsfsdfsdf", "dfsfdfs", "sdfdsf",]}></OptionBtnIn>
-            </div>
-            <div className='row justify-content-center mb-4'>
-              <button className="btn btn-lg btn-primary" disabled={this.state.state === appStates.not_ready} onClick={this.sendData}>Get Tagging</button>
-            </div>
+        <div className="bg-light mb-5">
+          <div className="container">
+            <section>
+              <div style={{height: '200px'}} className="mb-4"> 
+                <TextAreaIn onUpdate={this.updateData} name="data.content" placeholder="Input some data"></TextAreaIn>  
+              </div>
+              <div  className="mb-4">
+                <OptionBtnIn onUpdate={this.updateData} name="param.content" options={["sdfd", "sdsf", "sdfs", "dsfsfdfs", "dsfsdfsdf", "dfsfdfs", "sdfdsf", "dsdfsdf", "sdfd", "sdsf", "sdfs", "dsfsfdfs", "dsfsdfsdf", "dfsfdfs", "sdfdsf","sdfd", "sdsf", "sdfs", "dsfsfdfs", "dsfsdfsdf", "dfsfdfs", "sdfdsf","sdfd", "sdsf", "sdfs", "dsfsfdfs", "dsfsdfsdf", "dfsfdfs", "sdfdsf","sdfd", "sdsf", "sdfs", "dsfsfdfs", "dsfsdfsdf", "dfsfdfs", "sdfdsf","sdfd", "sdsf", "sdfs", "dsfsfdfs", "dsfsdfsdf", "dfsfdfs", "sdfdsf","sdfd", "sdsf", "sdfs", "dsfsfdfs", "dsfsdfsdf", "dfsfdfs", "sdfdsf","sdfd", "sdsf", "sdfs", "dsfsfdfs", "dsfsdfsdf", "dfsfdfs", "sdfdsf","sdfd", "sdsf", "sdfs", "dsfsfdfs", "dsfsdfsdf", "dfsfdfs", "sdfdsf","sdfd", "sdsf", "sdfs", "dsfsfdfs", "dsfsdfsdf", "dfsfdfs", "sdfdsf",]}></OptionBtnIn>
+              </div>
+              <div className='row justify-content-center mb-4'>
+                <button className="btn btn-lg btn-primary mb-5" disabled={this.state.state === appStates.not_ready} onClick={this.sendData}>Get Tagging</button>
+              </div>
+            </section>
+            { this.state.state === appStates.responded ?  
+              <section>
+                  <ClassificationDisplay classes={['NN', 'JJ', 'DT', 'UH', 'VBZ', 'RB'] }content={this.state.resp} onChange={this.updateResult}></ClassificationDisplay>
+                  <a ref={"data:text/json;charset=utf-8," + encodeURIComponent(JSON.stringify(this.state.resp))} download="result.json" className="btn btn-lg btn-primary mb-5">Download Result</a>
+              </section> : 
+              <div></div>
+            }
           </div>
-          { this.state.state === appStates.responded ?  
-            <div>
-                <ClassificationDisplay classes={['NN', 'JJ', 'DT', 'UH', 'VBZ', 'RB'] }content={this.state.resp} onChange={this.updateResult}></ClassificationDisplay>
-                <a ref={"data:text/json;charset=utf-8," + encodeURIComponent(JSON.stringify(this.state.resp))} download="result.json" className="btn btn-lg btn-primary">Download Result</a>
-            </div> : 
-            <div></div>
-          }
         </div>
+        <footer className="text-muted">
+          <div class="container">
+            A simple tagging demo created using demofi
+          </div>
+        </footer>
       </div>
     );
   }
