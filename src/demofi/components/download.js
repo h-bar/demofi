95e92e09e7c8de4a07f914b359042b05a8737b5e
Yan Zhang <663yan@gmail.com>
2020-09-06 21:31:32 -0400

    remove unused import

--- src/demofi/components/download.js
+++ src/demofi/components/download.js
@@ -2,7 +2,6 @@ import React from "react";
 import { useSelector } from "react-redux";
 import { selectResults } from "../slice";
 import { demofiDownloads } from "../../config";
-import { compose } from "@reduxjs/toolkit";
 
 const DownloadBtn = props => (
   <a href={"data:text/json;charset=utf-8," + props.downloadString} download={props.label + ".json"} className="btn btn-lg btn-primary mb-5 mx-2">
